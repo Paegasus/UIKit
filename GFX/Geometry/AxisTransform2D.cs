b96dd62a1efe90730fd35c3b6111fb1091f68125
Paegasus <lahcenebelbachir@gmail.com>
2026-02-21 02:52:32 +0100

    Changes.

--- GFX/Geometry/AxisTransform2D.cs
+++ GFX/Geometry/AxisTransform2D.cs
@@ -19,81 +19,81 @@ public struct AxisTransform2D
 {
     // Scale is applied before translation, i.e.
     // this->Transform(p) == scale_ * p + translation_
-    Vector2DF scale_;
-    Vector2DF translation_;
+    Vector2DF m_Scale;
+    Vector2DF m_Translation;
 
-    public readonly Vector2DF Scale => scale_;
-    public readonly Vector2DF Translation => translation_;
+    public readonly Vector2DF Scale => m_Scale;
+    public readonly Vector2DF Translation => m_Translation;
 
     public AxisTransform2D()
     {
-        scale_ = new(1.0f, 1.0f);
+        m_Scale = new(1.0f, 1.0f);
     }
 
     public AxisTransform2D(float scale, in Vector2DF translation)
     {
-        scale_ = new(scale, scale);
-        translation_ = translation;
+        m_Scale = new(scale, scale);
+        m_Translation = translation;
     }
 
-    private AxisTransform2D(in Vector2DF scale, in Vector2DF translation) => (scale_, translation_) = (scale, translation);
+    private AxisTransform2D(in Vector2DF scale, in Vector2DF translation) => (m_Scale, m_Translation) = (scale, translation);
 
     public static AxisTransform2D FromScaleAndTranslation(in Vector2DF scale, in Vector2DF translation) => new(scale, translation);
 
-    private readonly float MapX(float x) => ClampFloatGeometry(x * scale_.X + translation_.X);
+    private readonly float MapX(float x) => ClampFloatGeometry(x * m_Scale.X + m_Translation.X);
 
-    private readonly float MapY(float y) => ClampFloatGeometry(y * scale_.Y + translation_.Y);
-
-    // |* (1.0f / scale)| instead of '/ scale' to keep the same precision before crrev.com/c/3937107.
-    private readonly float InverseMapX(float x) => ClampFloatGeometry((x - translation_.X) * (1.0f / scale_.X));
+    private readonly float MapY(float y) => ClampFloatGeometry(y * m_Scale.Y + m_Translation.Y);
 
     // |* (1.0f / scale)| instead of '/ scale' to keep the same precision before crrev.com/c/3937107.
-    private readonly float InverseMapY(float y) => ClampFloatGeometry((y - translation_.Y) * (1.0f / scale_.Y));
+    private readonly float InverseMapX(float x) => ClampFloatGeometry((x - m_Translation.X) * (1.0f / m_Scale.X));
 
-    public void PreScale(in Vector2DF scale) => scale_.Scale(scale.X, scale.Y);
+    // |* (1.0f / scale)| instead of '/ scale' to keep the same precision before crrev.com/c/3937107.
+    private readonly float InverseMapY(float y) => ClampFloatGeometry((y - m_Translation.Y) * (1.0f / m_Scale.Y));
+
+    public void PreScale(in Vector2DF scale) => m_Scale.Scale(scale.X, scale.Y);
 
     public void PostScale(in Vector2DF scale)
     {
-        scale_.Scale(scale.X, scale.Y);
-        translation_.Scale(scale.X, scale.Y);
+        m_Scale.Scale(scale.X, scale.Y);
+        m_Translation.Scale(scale.X, scale.Y);
     }
 
-    public void PreTranslate(in Vector2DF translation) => translation_ += Vector2DF.ScaleVector2D(translation, scale_.X, scale_.Y);
+    public void PreTranslate(in Vector2DF translation) => m_Translation += Vector2DF.ScaleVector2D(translation, m_Scale.X, m_Scale.Y);
 
-    public void PostTranslate(in Vector2DF translation) => translation_ += translation;
+    public void PostTranslate(in Vector2DF translation) => m_Translation += translation;
 
     public void PreConcat(in AxisTransform2D pre)
     {
-        PreTranslate(pre.translation_);
-        PreScale(pre.scale_);
+        PreTranslate(pre.m_Translation);
+        PreScale(pre.m_Scale);
     }
     public void PostConcat(in AxisTransform2D post)
     {
-        PostScale(post.scale_);
-        PostTranslate(post.translation_);
+        PostScale(post.m_Scale);
+        PostTranslate(post.m_Translation);
     }
 
-    public readonly double Determinant() => (double)scale_.X * scale_.Y;
+    public readonly double Determinant() => (double)m_Scale.X * m_Scale.Y;
 
     // Check float determinant (stricter than checking each component or double
     // determinant) to keep consistency with Matrix44.
     // TODO(crbug.com/40237414): This may be stricter than necessary. Revisit
     // this after combination of gfx::Transform and blink::TransformationMatrix.
-    public readonly bool IsInvertible() => float.IsNormal(scale_.X * scale_.Y);
+    public readonly bool IsInvertible() => float.IsNormal(m_Scale.X * m_Scale.Y);
 
     public void Invert()
     {
 #if DEBUG
         Debug.Assert(IsInvertible());
 #endif
-        scale_ = new Vector2DF(1.0f / scale_.X, 1.0f / scale_.Y);
-        translation_.Scale(-scale_.X, -scale_.Y);
+        m_Scale = new Vector2DF(1.0f / m_Scale.X, 1.0f / m_Scale.Y);
+        m_Translation.Scale(-m_Scale.X, -m_Scale.Y);
     }
 
     // Changes the transform to: scale(z) * mat * scale(1/z).
     // Useful for mapping zoomed points to their zoomed transformed result:
     // new_mat * (scale(z) * x) == scale(z) * (mat * x).
-    public void Zoom(float zoom_factor) => translation_.Scale(zoom_factor);
+    public void Zoom(float zoom_factor) => m_Translation.Scale(zoom_factor);
 
     public readonly PointF MapPoint(in PointF p) => new(MapX(p.X), MapY(p.Y));
 
@@ -102,20 +102,20 @@ public struct AxisTransform2D
     public readonly RectF MapRect(in RectF r)
     {
 #if DEBUG
-        Debug.Assert(scale_.X >= 0.0f);
-        Debug.Assert(scale_.Y >= 0.0f);
+        Debug.Assert(m_Scale.X >= 0.0f);
+        Debug.Assert(m_Scale.Y >= 0.0f);
 #endif
-        return new RectF(MapX(r.X), MapY(r.Y), ClampFloatGeometry(r.Width * scale_.X), ClampFloatGeometry(r.Height * scale_.Y));
+        return new RectF(MapX(r.X), MapY(r.Y), ClampFloatGeometry(r.Width * m_Scale.X), ClampFloatGeometry(r.Height * m_Scale.Y));
     }
 
     public readonly RectF InverseMapRect(in RectF r)
     {
 #if DEBUG
-        Debug.Assert(scale_.X > 0.0f);
-        Debug.Assert(scale_.Y > 0.0f);
+        Debug.Assert(m_Scale.X > 0.0f);
+        Debug.Assert(m_Scale.Y > 0.0f);
 #endif
         // |* (1.0f / scale)| instead of '/ scale' to keep the same precision before crrev.com/c/3937107.
-        return new RectF(InverseMapX(r.X), InverseMapY(r.Y), ClampFloatGeometry(r.Width * (1.0f / scale_.X)), ClampFloatGeometry(r.Height * (1.0f / scale_.Y)));
+        return new RectF(InverseMapX(r.X), InverseMapY(r.Y), ClampFloatGeometry(r.Width * (1.0f / m_Scale.X)), ClampFloatGeometry(r.Height * (1.0f / m_Scale.Y)));
     }
 
     // Decomposes this transform into |decomp|, following the 2d decomposition
@@ -125,19 +125,19 @@ public struct AxisTransform2D
     {
         DecomposedTransform decomp = new();
 
-        decomp.Translate.X = translation_.X;
-        decomp.Translate.Y = translation_.Y;
+        decomp.Translate.X = m_Translation.X;
+        decomp.Translate.Y = m_Translation.Y;
 
-        if (scale_.X >= 0 || scale_.Y >= 0)
+        if (m_Scale.X >= 0 || m_Scale.Y >= 0)
         {
-            decomp.Scale.X = scale_.X;
-            decomp.Scale.Y = scale_.Y;
+            decomp.Scale.X = m_Scale.X;
+            decomp.Scale.Y = m_Scale.Y;
         }
         else
         {
             // If both scales are negative, decompose to positive scales with a 180deg rotation.
-            decomp.Scale.X = -scale_.X;
-            decomp.Scale.Y = -scale_.Y;
+            decomp.Scale.X = -m_Scale.X;
+            decomp.Scale.Y = -m_Scale.Y;
             decomp.Quaternion.Z = 1;
             decomp.Quaternion.W = 0;
         }
@@ -186,5 +186,5 @@ public struct AxisTransform2D
         return result;
     }
 
-    public override readonly string ToString() => $"[{scale_}, {translation_}]";
+    public override readonly string ToString() => $"[{m_Scale}, {m_Translation}]";
 }
