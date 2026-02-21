886d9734d9fe2d95ecb36f9532b749ef7553130f
Paegasus <lahcenebelbachir@gmail.com>
2026-02-21 03:00:47 +0100

    Changes.

--- Tests/AxisTransform2DTest.cs
+++ Tests/AxisTransform2DTest.cs
@@ -22,12 +22,11 @@ public static class AxisTransform2DTest
         AxisTransform2D t = new(1.25f, new Vector2DF(3.75f, 55.0f));
 
         PointF p = new(150.0f, 100.0f);
-
         Debug.Assert(new PointF(191.25f, 180.0f) == t.MapPoint(p));
-        //EXPECT_POINTF_EQ(PointF(117.f, 36.f), t.InverseMapPoint(p));
+        Debug.Assert(new PointF(117.0f, 36.0f) == t.InverseMapPoint(p));
 
-        //RectF r(150.f, 100.f, 22.5f, 37.5f);
-        //EXPECT_EQ(RectF(191.25f, 180.f, 28.125f, 46.875f), t.MapRect(r));
-        //EXPECT_RECTF_EQ(RectF(117.f, 36.f, 18.f, 30.f), t.InverseMapRect(r));
+        RectF r = new(150.0f, 100.0f, 22.5f, 37.5f);
+        Debug.Assert(new RectF(191.25f, 180.0f, 28.125f, 46.875f) == t.MapRect(r));
+        Debug.Assert(new RectF(117.0f, 36.0f, 18.0f, 30.0f) == t.InverseMapRect(r));
     }
 }
