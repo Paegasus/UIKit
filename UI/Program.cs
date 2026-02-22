6c68b11d43567369de56a733b7b75d36cc15eb13
Paegasus <lahcenebelbachir@gmail.com>
2026-02-22 23:20:05 +0100

    Changes.

--- UI/Program.cs
+++ UI/Program.cs
@@ -10,6 +10,14 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        
+        Matrix44 matrix = new ();
+
+        Debug.WriteLine($"matrix1 IsIdentity: {matrix.IsIdentity}");
+        Debug.WriteLine($"matrix1 IsIdentityOrTranslation: {matrix.IsIdentityOrTranslation}");
+        Debug.WriteLine($"matrix1 IsScaleOrTranslation: {matrix.IsScaleOrTranslation}");
+        Debug.WriteLine($"matrix1 IsScale: {matrix.IsScale}");
+        Debug.WriteLine($"matrix1 IsFlat: {matrix.IsFlat}");
+        Debug.WriteLine($"matrix1 HasPerspective: {matrix.HasPerspective}");
+        Debug.WriteLine($"matrix1 Is2DTransform: {matrix.Is2DTransform}");
     }
 }
