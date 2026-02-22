f155228800ca743af58a92df45ba37bc437c6f6d
Paegasus <lahcenebelbachir@gmail.com>
2026-02-23 00:13:13 +0100

    Changes.

--- UI Tests/Tests/Vector2DTest.cs
+++ UI Tests/Tests/Vector2DTest.cs
@@ -1,6 +1,6 @@
-using System.Diagnostics;
-using UI.GFX.Geometry;
 using Xunit;
+
+using UI.GFX.Geometry;
 
 namespace UI.Tests;
 
@@ -37,6 +37,7 @@ public static class Vector2DTest
         Assert.Equal(new Vector2D(3, -3), -new Vector2D(-3, 3));
     }
 
+    [Fact]
     private static void TestLength()
     {
         (int X, int Y)[] values =
@@ -54,38 +55,40 @@ public static class Vector2DTest
             double length_squared =  (double)X * X + (double)Y * Y;
             double length = Math.Sqrt(length_squared);
             Vector2D vector = new(X, Y);
-            Debug.Assert((float)length_squared == vector.LengthSquared());
-            Debug.Assert((float)length == vector.Length());
+            Assert.Equal((float)length_squared, vector.LengthSquared());
+            Assert.Equal((float)length, vector.Length());
         }
     }
 
+    [Fact]
     private static void TestSetToMinMax()
     {
         Vector2D a;
 
         a = new Vector2D(3, 5);
-        Debug.Assert(new Vector2D(3, 5) == a);
+        Assert.Equal(new Vector2D(3, 5), a);
         a.SetToMax(new Vector2D(2, 4));
-        Debug.Assert(new Vector2D(3, 5) == a);
+        Assert.Equal(new Vector2D(3, 5), a);
         a.SetToMax(new Vector2D(3, 5));
-        Debug.Assert(new Vector2D(3, 5) == a);
+        Assert.Equal(new Vector2D(3, 5), a);
         a.SetToMax(new Vector2D(4, 2));
-        Debug.Assert(new Vector2D(4, 5) == a);
+        Assert.Equal(new Vector2D(4, 5), a);
         a.SetToMax(new Vector2D(8, 10));
-        Debug.Assert(new Vector2D(8, 10) == a);
+        Assert.Equal(new Vector2D(8, 10), a);
 
         a.SetToMin(new Vector2D(9, 11));
-        Debug.Assert(new Vector2D(8, 10) == a);
+        Assert.Equal(new Vector2D(8, 10), a);
         a.SetToMin(new Vector2D(8, 10));
-        Debug.Assert(new Vector2D(8, 10) == a);
+        Assert.Equal(new Vector2D(8, 10), a);
         a.SetToMin(new Vector2D(11, 9));
-        Debug.Assert(new Vector2D(8, 9) == a);
+        Assert.Equal(new Vector2D(8, 9), a);
         a.SetToMin(new Vector2D(7, 11));
-        Debug.Assert(new Vector2D(7, 9) == a);
+        Assert.Equal(new Vector2D(7, 9), a);
         a.SetToMin(new Vector2D(3, 5));
-        Debug.Assert(new Vector2D(3, 5) == a);
+        Assert.Equal(new Vector2D(3, 5), a);
     }
 
+    [Fact]
     private static void TestIntegerOverflow()
     {
         int int_max = int.MaxValue;
@@ -97,49 +100,50 @@ public static class Vector2DTest
 
         test = new Vector2D();
         test += new Vector2D(int_max, int_max);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
 
         test = new Vector2D();
         test += new Vector2D(int_min, int_min);
-        Debug.Assert(test == min_vector);
+        Assert.Equal(test,  min_vector);
 
         test = new Vector2D(10, 20);
         test += new Vector2D(int_max, int_max);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
 
         test = new Vector2D(-10, -20);
         test += new Vector2D(int_min, int_min);
-        Debug.Assert(test == min_vector);
+        Assert.Equal(test,  min_vector);
 
         test = new Vector2D();
         test -= new Vector2D(int_max, int_max);
-        Debug.Assert(test == new Vector2D(-int_max, -int_max));
+        Assert.Equal(test,  new Vector2D(-int_max, -int_max));
 
         test = new Vector2D();
         test -= new Vector2D(int_min, int_min);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
 
         test = new Vector2D(10, 20);
         test -= new Vector2D(int_min, int_min);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
 
         test = new Vector2D(-10, -20);
         test -= new Vector2D(int_max, int_max);
-        Debug.Assert(test == min_vector);
+        Assert.Equal(test,  min_vector);
 
         test = new Vector2D();
         test -= new Vector2D(int_min, int_min);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
 
         test = -new Vector2D(int_min, int_min);
-        Debug.Assert(test == max_vector);
+        Assert.Equal(test,  max_vector);
     }
 
+    [Fact]
     private static void TestTranspose()
     {
         Vector2D v = new(1, -2);
-        Debug.Assert(new Vector2D(-2, 1) == Vector2D.TransposeVector2D(v));
+        Assert.Equal(new Vector2D(-2, 1), Vector2D.TransposeVector2D(v));
         v.Transpose();
-        Debug.Assert(new Vector2D(-2, 1) == v);
+        Assert.Equal(new Vector2D(-2, 1), v);
     }
 }
