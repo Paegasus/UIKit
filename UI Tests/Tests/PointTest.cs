5fcaaec004d7168d52f885cc666b74e8e870ea57
Paegasus <lahcenebelbachir@gmail.com>
2026-02-24 22:49:39 +0100

    Changes.

--- UI Tests/Tests/PointTest.cs
+++ UI Tests/Tests/PointTest.cs
@@ -43,4 +43,40 @@ public static class PointTest
             Assert.Equal(expected, actual);
         }
     }
+
+    [Fact]
+    private static void TestOffsetFromPoint()
+    {
+        Point a = new(1, 5);
+        Point b = new(-20, 8);
+        Assert.Equal(new Vector2D(-20 - 1, 8 - 5), (b - a));
+    }
+
+    [Fact]
+    private static void TestSetToMinMax()
+    {
+        Point a;
+
+        a = new Point(3, 5);
+        Assert.Equal(new Point(3, 5), a);
+        a.SetToMax(new Point(2, 4));
+        Assert.Equal(new Point(3, 5), a);
+        a.SetToMax(new Point(3, 5));
+        Assert.Equal(new Point(3, 5), a);
+        a.SetToMax(new Point(4, 2));
+        Assert.Equal(new Point(4, 5), a);
+        a.SetToMax(new Point(8, 10));
+        Assert.Equal(new Point(8, 10), a);
+
+        a.SetToMin(new Point(9, 11));
+        Assert.Equal(new Point(8, 10), a);
+        a.SetToMin(new Point(8, 10));
+        Assert.Equal(new Point(8, 10), a);
+        a.SetToMin(new Point(11, 9));
+        Assert.Equal(new Point(8, 9), a);
+        a.SetToMin(new Point(7, 11));
+        Assert.Equal(new Point(7, 9), a);
+        a.SetToMin(new Point(3, 5));
+        Assert.Equal(new Point(3, 5), a);
+    }
 }
