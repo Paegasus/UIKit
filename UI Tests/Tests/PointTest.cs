06d460741ea984152ca784264054af4f84043fa0
Paegasus <lahcenebelbachir@gmail.com>
2026-02-24 22:55:33 +0100

    Changes.

--- UI Tests/Tests/PointTest.cs
+++ UI Tests/Tests/PointTest.cs
@@ -79,4 +79,92 @@ public static class PointTest
         a.SetToMin(new Point(3, 5));
         Assert.Equal(new Point(3, 5), a);
     }
+
+    [Fact]
+    private static void TestOffset()
+    {
+        Point test = new(3, 4);
+        test.Offset(5, -8);
+        Assert.Equal(test, new Point(8, -4));
+    }
+
+    [Fact]
+    private static void TestVectorMath()
+    {
+        Point test = new(3, 4);
+        test += new Vector2D(5, -8);
+        Assert.Equal(test, new Point(8, -4));
+
+        Point test2 = new Point(3, 4);
+        test2 -= new Vector2D(5, -8);
+        Assert.Equal(test2, new Point(-2, 12));
+    }
+
+    [Fact]
+    private static void TestIntegerOverflow()
+    {
+        int int_max = int.MaxValue;
+        int int_min = int.MinValue;
+
+        Point max_point = new(int_max, int_max);
+        Point min_point = new(int_min, int_min);
+        Point test;
+
+        test = new Point();
+        test.Offset(int_max, int_max);
+        Assert.Equal(test, max_point);
+
+        test = new Point();
+        test.Offset(int_min, int_min);
+        Assert.Equal(test, min_point);
+
+        test = new Point(10, 20);
+        test.Offset(int_max, int_max);
+        Assert.Equal(test, max_point);
+
+        test = new Point(-10, -20);
+        test.Offset(int_min, int_min);
+        Assert.Equal(test, min_point);
+
+        test = new Point();
+        test += new Vector2D(int_max, int_max);
+        Assert.Equal(test, max_point);
+
+        test = new Point();
+        test += new Vector2D(int_min, int_min);
+        Assert.Equal(test, min_point);
+
+        test = new Point(10, 20);
+        test += new Vector2D(int_max, int_max);
+        Assert.Equal(test, max_point);
+
+        test = new Point(-10, -20);
+        test += new Vector2D(int_min, int_min);
+        Assert.Equal(test, min_point);
+
+        test = new Point();
+        test -= new Vector2D(int_max, int_max);
+        Assert.Equal(test, new Point(-int_max, -int_max));
+
+        test = new Point();
+        test -= new Vector2D(int_min, int_min);
+        Assert.Equal(test, max_point);
+
+        test = new Point(10, 20);
+        test -= new Vector2D(int_min, int_min);
+        Assert.Equal(test, max_point);
+
+        test = new Point(-10, -20);
+        test -= new Vector2D(int_max, int_max);
+        Assert.Equal(test, min_point);
+    }
+
+    [Fact]
+    private static void TestTranspose()
+    {
+        Point p = new(1, -2);
+        Assert.Equal(new Point(-2, 1), Point.TransposePoint(p));
+        p.Transpose();
+        Assert.Equal(new Point(-2, 1), p);
+    }
 }
