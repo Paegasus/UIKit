394752e9beab3a5c276a0c3cdbdabc20b072a4c0
Paegasus <lahcenebelbachir@gmail.com>
2026-02-28 13:29:51 +0100

    Changes.

--- UI Tests/Tests/MaskFilterInfoTest.cs
+++ UI Tests/Tests/MaskFilterInfoTest.cs
@@ -6,9 +6,20 @@ namespace UI.Tests;
 
 public static class MaskFilterInfoTest
 {
-    [Fact]
-    private static void Test()
+
+    private static LinearGradient CreateGradient(short angle)
     {
-        
+        LinearGradient gradient = new(angle);
+        gradient.AddStep(0.5f, 50);
+        return gradient;
+    }
+
+    [Fact]
+    private static void TestApplyTransform()
+    {
+        var info = new MaskFilterInfo(new RRectF(1.0f, 2.0f, 20.0f, 25.0f, 5.0f));
+        MaskFilterInfo expected = info;
+        info.ApplyTransform(new Transform());
+        Assert.Equal(expected, info);
     }
 }
