df3a011360edc154009655c686df4a262fcbc39b
Paegasus <lahcenebelbachir@gmail.com>
2026-03-02 04:26:41 +0100

    Changes.

--- UI Tests/Tests/Matrix3FTest.cs
+++ UI Tests/Tests/Matrix3FTest.cs
@@ -68,13 +68,63 @@ public static class Matrix3FTest
         Assert.Equal(0.3149f, matrix.Determinant(), 0.0001f);
     }
 
-    private static void Test1()
+    [Fact]
+    private static void TestInverse()
     {
+        Matrix3F identity = Matrix3F.Identity();
+        Matrix3F inv_identity = identity.Inverse();
+        Assert.Equal(identity, inv_identity);
 
+        Matrix3F singular = Matrix3F.Zeros();
+        singular.Set(1.0f, 3.0f, 4.0f,
+                     2.0f, 11.0f, 5.0f,
+                     0.5f, 1.5f, 2.0f);
+        Assert.Equal(0, singular.Determinant());
+        Assert.Equal(Matrix3F.Zeros(), singular.Inverse());
+
+        Matrix3F regular = Matrix3F.Zeros();
+        regular.Set(0.57f, 0.205f, 0.942f,
+                    0.314f, 0.845f, 0.826f,
+                    0.131f, 0.025f, 0.962f);
+        Matrix3F inv_regular = regular.Inverse();
+        regular.Set(2.51540616f, -0.55138018f, -1.98968043f,
+                    -0.61552266f, 1.34920184f, -0.55573636f,
+                    -0.32653861f, 0.04002158f, 1.32488726f);
+        Assert.True(regular.IsNear(inv_regular, 0.00001f));
     }
 
-    private static void Test2()
+    [Fact]
+    private static void TestTranspose()
     {
+        Matrix3F matrix = Matrix3F.Zeros();
 
+        matrix.Set(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+
+        Matrix3F transpose = matrix.Transpose();
+        Assert.Equal(new Vector3DF(0.0f, 1.0f, 2.0f), transpose.GetColumn(0));
+        Assert.Equal(new Vector3DF(3.0f, 4.0f, 5.0f), transpose.GetColumn(1));
+        Assert.Equal(new Vector3DF(6.0f, 7.0f, 8.0f), transpose.GetColumn(2));
+
+        Assert.True(matrix.IsEqual(transpose.Transpose()));
+    }
+
+    [Fact]
+    private static void TestOperators()
+    {
+        Matrix3F matrix1 = Matrix3F.Zeros();
+        matrix1.Set(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Assert.Equal(matrix1 + Matrix3F.Zeros(), matrix1);
+
+        Matrix3F matrix2 = Matrix3F.Zeros();
+        matrix2.Set(-1, -2, -3, -4, -5, -6, -7, -8, -9);
+        Assert.Equal(matrix1 + matrix2, Matrix3F.Zeros());
+
+        Assert.Equal(Matrix3F.Zeros() - matrix1, matrix2);
+
+        Matrix3F result = Matrix3F.Zeros();
+        result.Set(2, 4, 6, 8, 10, 12, 14, 16, 18);
+        Assert.Equal(matrix1 - matrix2, result);
+        result.Set(-2, -4, -6, -8, -10, -12, -14, -16, -18);
+        Assert.Equal(matrix2 - matrix1, result);
     }
 }
