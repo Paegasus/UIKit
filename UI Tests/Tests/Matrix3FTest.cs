64d6781e1a45d5f9826ebf3c99c6261368cb5964
Paegasus <lahcenebelbachir@gmail.com>
2026-03-02 04:14:00 +0100

    Changes.

---
+++ UI Tests/Tests/Matrix3FTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+using UI.GFX.Geometry;
+using System.Diagnostics;
+
+namespace UI.Tests;
+
+public static class Matrix3FTest
+{
+  [Fact]
+  private static void Test()
+  {
+   
+  }
+}
