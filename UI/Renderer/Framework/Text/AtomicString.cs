94c529b974a42e382c731f1398e7955756cfc742
Paegasus <lahcenebelbachir@gmail.com>
2026-03-06 17:13:19 +0100

    Changes.

--- UI/Renderer/Framework/Text/AtomicString.cs
+++ UI/Renderer/Framework/Text/AtomicString.cs
@@ -12,15 +12,17 @@ public readonly struct AtomicString // : IEquatable<AtomicString>
     public bool IsNull  => _value == null;
     public bool IsEmpty => _value == null || _value.Length == 0;
 
+    public override string? ToString() => _value;
+    
+    public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+
     // Fast pointer comparison since both sides are interned
     public bool Equals(in AtomicString other) => ReferenceEquals(_value, other._value);
+    public override bool Equals(object? obj) => obj is AtomicString other && Equals(other);
+
+    public static bool operator ==(in AtomicString a, in AtomicString b) => a.Equals(b);
+    public static bool operator !=(in AtomicString a, in AtomicString b) => !a.Equals(b);
 
     public static implicit operator string?(AtomicString s) => s._value;
     public static explicit operator AtomicString(string? s) => new(s);
-
-    public override bool Equals(object? obj) => obj is AtomicString other && Equals(other);
-    public override int GetHashCode() => _value?.GetHashCode() ?? 0;
-    public static bool operator ==(in AtomicString a, in AtomicString b) => a.Equals(b);
-    public static bool operator !=(in AtomicString a, in AtomicString b) => !a.Equals(b);
-    public override string? ToString() => _value;
 }
