3c7757d2f2e1ffd81c13c0205d23f15a4eae5235
Paegasus <lahcenebelbachir@gmail.com>
2026-03-06 22:01:30 +0100

    Changes.

--- UI/Renderer/Core/CSS/CSSStyleSheet.cs
+++ UI/Renderer/Core/CSS/CSSStyleSheet.cs
@@ -4,5 +4,33 @@ namespace UI.Renderer.Core.CSS;
 
 public sealed class CSSStyleSheet : StyleSheet, IMediaQuerySetOwner
 {
+    // StyleSheet implementation
     
+    public override bool Disabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public override Node? OwnerNode => throw new NotImplementedException();
+
+    public override string Href => throw new NotImplementedException();
+
+    public override string Title => throw new NotImplementedException();
+
+    public override string Type => throw new NotImplementedException();
+
+    public override Uri BaseUrl => throw new NotImplementedException();
+
+    public override bool IsLoading => throw new NotImplementedException();
+
+    public override void ClearOwnerNode()
+    {
+        throw new NotImplementedException();
+    }
+
+    // IMediaQuerySetOwner implementation
+
+    public MediaQuerySet? MediaQueries => throw new NotImplementedException();
+
+    public void SetMediaQueries(MediaQuerySet? mediaQueries)
+    {
+        throw new NotImplementedException();
+    }
 }
